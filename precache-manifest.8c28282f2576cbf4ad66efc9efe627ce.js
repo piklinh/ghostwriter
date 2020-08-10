13c02393668d7550c20e93fdea66ec4a91814846
piklin <hplesson@gmail.com>
2020-08-10 16:42:05 -0400

    Updates

---
+++ precache-manifest.8c28282f2576cbf4ad66efc9efe627ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6e0682f5f95a7a82115752f1a3806f",
+    "url": "/piklinHoeProjectFive/index.html"
+  },
+  {
+    "revision": "f8d7e91dea3333dfa6f0",
+    "url": "/piklinHoeProjectFive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25f004535fba27b4e2c1",
+    "url": "/piklinHoeProjectFive/static/js/2.8055ad02.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/piklinHoeProjectFive/static/js/2.8055ad02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d7e91dea3333dfa6f0",
+    "url": "/piklinHoeProjectFive/static/js/main.996378be.chunk.js"
+  },
+  {
+    "revision": "977ebc22706f4e7e7db0",
+    "url": "/piklinHoeProjectFive/static/js/runtime-main.338235ce.js"
+  }
+]);
